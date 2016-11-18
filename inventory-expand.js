6e90278f2ee1de2136f7196746994bdbd9504de0
rektide de la faye <rektide@voodoowarez.com>
2016-11-18 17:23:41 -0500

    Add left-pad capabilities.

--- inventory-expand.js
+++ inventory-expand.js
@@ -1,19 +1,27 @@
 #!/usr/bin/env node
 "use strict"
-var byline= require("byline")
+var
+  byline= require("byline"),
+  pad= require("left-pad")
 
-var suffix = process.argv[2]
-var match = /^(\w*)\[(\d+):(\d+)\]([\w_\.]*)$/
+var
+  suffix= process.argv[2]|| "",
+  match= /^(\w*)\[(\d+):(\d+)\]([\w_\.]*)$/
 
 byline(process.stdin, {encoding: "utf8"}).on("data", function(line){
 	var range= match.exec(line)
 	if(!range){
-		console.log(line)
+		console.log(line+ suffix)
 		return
 	}
 
-	var lead= range[1], start= parseInt(range[2]), end= parseInt(range[3]), tail= range[4]
+	var
+	  lead= range[1],
+	  start= parseInt(range[2]),
+	  end= parseInt(range[3]),
+	  tail= range[4],
+	  len= range[2].length
 	for(var i= start; i<= end; ++i){
-		console.log(lead+ i+ tail)
+		console.log(lead+ pad(i, len, 0)+ tail+ suffix)
 	}
 })
